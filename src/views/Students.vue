608f877a9e453a998a67b5ac1d23b75be328de09
dgnytrnga <dgnytrnga@gmail.com>
2024-06-11 17:00:16 +0300

    first commit

---
+++ src/views/Students.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Öğrenciler</h1>
+    <div v-for="student in students" :key="student.id">
+        <p>{{student.name}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+        students: [
+            {id:1, name: "Leo", not: 55},
+            {id:2, name: "Adam", not: 75},
+            {id:3, name: "Seth", not: 35}
+    ]}
+        
+}
+
+}
+</script>
+
+<style>
+
+</style>
